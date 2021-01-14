0d6d06c32565086dcc5c440217bb1d5cfe543a37
Felipe C. Araujo <felipe.s-04@hotmail.com>
2021-01-14 14:41:16 -0300

    ensure RemoteAuthentication throws UnexpectedError on 200 with invalid data

--- lib/data/models/remote_account.model.dart
+++ lib/data/models/remote_account.model.dart
@@ -1,10 +1,17 @@
 import '../../domain/entities/entities.dart';
 
+import '../../data/http/http.dart';
+
 class RemoteAccountModel {
   final String accessToken;
   RemoteAccountModel(this.accessToken);
 
-  factory RemoteAccountModel.fromJson(Map json) => RemoteAccountModel(json['accessToken']);
+  factory RemoteAccountModel.fromJson(Map json) {
+    if(!json.containsKey('accessToken')){
+      throw HttpError.invalidData;
+    }
+    return RemoteAccountModel(json['accessToken']);
+  }
 
   AccountEntity toEntity() => AccountEntity(accessToken);
 }
--- test/data/usecases/remote_authentication_test.dart
+++ test/data/usecases/remote_authentication_test.dart
@@ -85,4 +85,16 @@ void main() {
     final account = await sut.auth(params);
     expect(account.token, accessToken);
   });
+
+  test('Should throw UnexpectedError if HttpClient return 200 with invalid data', () async {
+    when(httpClient.request(
+        url: anyNamed('url'),
+        method: anyNamed('method'),
+        body: anyNamed('body')))
+        .thenAnswer((_) async =>
+    {'invalid_key': 'invalid_value'});
+
+    final future = sut.auth(params);
+    expect(future, throwsA(DomainError.unexpected));
+  });
 }
