2e60befb6fcf04b555eb32a6d3a00da7af56bef4
Felipe C. Araujo <felipe.s-04@hotmail.com>
2021-01-14 16:45:17 -0300

    ensure HttpAdapter return null if post return 200 no data

--- test/infra/http/http_adapter_test.dart
+++ test/infra/http/http_adapter_test.dart
@@ -20,7 +20,7 @@ class HttpAdapter implements HttpClient{
     };
     final jsonBody = body != null ? jsonEncode(body) : null;
     final response = await client.post(url, headers: headers,body: jsonBody);
-    return jsonDecode(response.body);
+    return response.body.isEmpty ? null : jsonDecode(response.body);
   }
 }
 
@@ -61,6 +61,12 @@ void main() {
       when(client.post(any,headers: anyNamed('headers'))).thenAnswer((_) async => Response('{"any_key":"any_value"}',200));
       final response = await sut.request(url: url, method: 'post');
       expect(response, {"any_key":"any_value"});
+    });
+
+    test('Should return null if post return 200 with no data', () async {
+      when(client.post(any,headers: anyNamed('headers'))).thenAnswer((_) async => Response('',200));
+      final response = await sut.request(url: url, method: 'post');
+      expect(response, null);
     });
 
   });
