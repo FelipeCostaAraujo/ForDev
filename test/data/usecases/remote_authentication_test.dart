94adf68863fa64daef8b4d71d1263ba68160840f
Felipe C. Araujo <felipe.s-04@hotmail.com>
2021-01-14 13:46:22 -0300

    ensure RemoteAuthentication throws InvalidCredentialsError on 401

--- test/data/usecases/remote_authentication_test.dart
+++ test/data/usecases/remote_authentication_test.dart
@@ -48,11 +48,11 @@ void main() {
     expect(future, throwsA(DomainError.unexpected));
   });
 
-  test('Should throw UnexpectedError if HttpClient return 500', () async {
+  test('Should throw InvalidCredentialsError if HttpClient return 401', () async {
     when(httpClient.request(url: anyNamed('url'), method: anyNamed('method'),body: anyNamed('body')))
-        .thenThrow(HttpError.serverError);
+        .thenThrow(HttpError.unauthorized);
 
     final future =  sut.auth(params);
-    expect(future, throwsA(DomainError.unexpected));
+    expect(future, throwsA(DomainError.invalidCredentials));
   });
 }
