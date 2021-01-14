7d675515f753d9924224547ceb8996a1251fbd1a
Felipe C. Araujo <felipe.s-04@hotmail.com>
2021-01-14 16:25:09 -0300

    ensure HttpAdapter calls post without body

--- test/infra/http/http_adapter_test.dart
+++ test/infra/http/http_adapter_test.dart
@@ -17,7 +17,8 @@ class HttpAdapter {
       'content-type': 'application/json',
       'accept': 'application/json'
     };
-    await client.post(url, headers: headers,body: jsonEncode(body));
+    final jsonBody = body != null ? jsonEncode(body) : null;
+    await client.post(url, headers: headers,body: jsonBody);
   }
 }
 
@@ -43,5 +44,14 @@ void main() {
         'accept': 'application/json'
       }, body: '{"any_key":"any_value"}'));
     });
+
+    test('Should call post without body', () async {
+      await sut.request(url: url, method: 'post');
+      verify(client.post(
+          any,
+          headers: anyNamed('headers')
+      ));
+    });
+    
   });
 }
