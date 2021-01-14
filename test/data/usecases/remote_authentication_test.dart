366be6839f9d959fe4d10aa17d81d9ae425d7736
Felipe C. Araujo <felipe.s-04@hotmail.com>
2021-01-14 13:39:32 -0300

    ensure RemoteAuthentication throws UnexpectedError on 405

--- test/data/usecases/remote_authentication_test.dart
+++ test/data/usecases/remote_authentication_test.dart
@@ -47,4 +47,12 @@ void main() {
     final future =  sut.auth(params);
     expect(future, throwsA(DomainError.unexpected));
   });
+
+  test('Should throw UnexpectedError if HttpClient return 500', () async {
+    when(httpClient.request(url: anyNamed('url'), method: anyNamed('method'),body: anyNamed('body')))
+        .thenThrow(HttpError.serverError);
+
+    final future =  sut.auth(params);
+    expect(future, throwsA(DomainError.unexpected));
+  });
 }
