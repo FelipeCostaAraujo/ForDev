8c5720d3053f6f784fe45a6bcc4a92a50fc5bffa
Felipe C. Araujo <felipe.s-04@hotmail.com>
2021-01-14 16:18:20 -0300

    ensure HttpAdapter calls post with correct Body

--- test/infra/http/http_adapter_test.dart
+++ test/infra/http/http_adapter_test.dart
@@ -1,3 +1,5 @@
+import 'dart:convert';
+
 import 'package:faker/faker.dart';
 import 'package:http/http.dart';
 import 'package:test/test.dart';
@@ -9,41 +11,37 @@ class HttpAdapter {
   final Client client;
   HttpAdapter(this.client);
 
-  Future<void> request({
-    @required String url,
-    @required String method,
-    Map body
-  }) async {
+  Future<void> request(
+      {@required String url, @required String method, Map body}) async {
     final headers = {
-      'content-type':'application/json',
-      'accept':'application/json'
+      'content-type': 'application/json',
+      'accept': 'application/json'
     };
-    await client.post(url,headers: headers);
+    await client.post(url, headers: headers,body: jsonEncode(body));
   }
 }
 
 class ClientSpy extends Mock implements Client {}
 
-void main(){
+void main() {
   HttpAdapter sut;
   ClientSpy client;
   String url;
 
-  setUp((){
+  setUp(() {
     client = ClientSpy();
     sut = HttpAdapter(client);
     url = faker.internet.httpUrl();
   });
 
-  group('post',(){
-
-    test('Should call post with correct value',() async {
-      await sut.request(url: url, method: 'post');
+  group('post', () {
+    test('Should call post with correct value', () async {
+      await sut
+          .request(url: url, method: 'post', body: {'any_key': 'any_value'});
       verify(client.post(url, headers: {
-        'content-type':'application/json',
-        'accept':'application/json'
-      }));
+        'content-type': 'application/json',
+        'accept': 'application/json'
+      }, body: '{"any_key":"any_value"}'));
     });
-
   });
 }
