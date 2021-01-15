7bfefb9888b17639659d99372089724ea42645b7
Felipe C. Araujo <felipe.s-04@hotmail.com>
2021-01-15 18:22:49 -0300

    ensure LoginPage calls authentication on form submit

--- test/ui/pages/login_page_test.dart
+++ test/ui/pages/login_page_test.dart
@@ -117,5 +117,13 @@ void main() {
     expect(button.onPressed, null);
   });
 
+  testWidgets('Should call authentication on form submit', (WidgetTester tester) async {
+    await loadPage(tester);
+    isFormValidController.add(true);
+    await tester.pump();
+    await tester.tap(find.byType(RaisedButton));
+    await tester.pump();
+    verify(presenter.auth()).called(1);
+  });
 
 }
