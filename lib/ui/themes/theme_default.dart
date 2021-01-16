1d1f85b66f8565b8f661a1c72bd322036ff9e405
Felipe C. Araujo <felipe.s-04@hotmail.com>
2021-01-16 01:04:25 -0300

    ensure StreamLoginPresneter calls Validation on email changed

--- lib/ui/themes/theme_default.dart
+++ lib/ui/themes/theme_default.dart
@@ -10,38 +10,31 @@ ThemeData themeDefault() {
 
   SystemChrome.setSystemUIOverlayStyle(SystemUiOverlayStyle.light);
   return ThemeData(
-    brightness: brightness,
-    inputDecorationTheme: InputDecorationTheme(
-      enabledBorder: UnderlineInputBorder(
-        borderSide: BorderSide(color: primaryColorLight)
-      ),
-      focusedBorder: UnderlineInputBorder(
-          borderSide: BorderSide(color: primaryColor)
-      ),
-      alignLabelWithHint: true
-    ),
-    primaryColor: primaryColor,
-    primaryColorDark: primaryColorDark,
-    primaryColorLight: primaryColorLight,
-    backgroundColor: backgroundColor,
-    accentColor: primaryColor,
-    textTheme: TextTheme(
-      headline1: TextStyle(
-        fontSize: 30,
-        fontWeight: FontWeight.bold,
-        color: primaryColorDark
-      )
-    ),
-    buttonTheme: ButtonThemeData(
-      colorScheme: ColorScheme.light(primary: primaryColor),
-      buttonColor: primaryColor,
-      splashColor: primaryColorLight,
-      padding: EdgeInsets.symmetric(vertical: 10,horizontal: 20),
-      textTheme: ButtonTextTheme.primary,
-      shape: RoundedRectangleBorder(
-        borderRadius: BorderRadius.circular(20)
-      )
-    )
-    //fontFamily: 'Montserrat',
-  );
+      brightness: brightness,
+      inputDecorationTheme: InputDecorationTheme(
+          enabledBorder: UnderlineInputBorder(
+              borderSide: BorderSide(color: primaryColorLight)),
+          focusedBorder:
+              UnderlineInputBorder(borderSide: BorderSide(color: primaryColor)),
+          alignLabelWithHint: true),
+      primaryColor: primaryColor,
+      primaryColorDark: primaryColorDark,
+      primaryColorLight: primaryColorLight,
+      backgroundColor: backgroundColor,
+      accentColor: primaryColor,
+      textTheme: TextTheme(
+          headline1: TextStyle(
+              fontSize: 30,
+              fontWeight: FontWeight.bold,
+              color: primaryColorDark)),
+      buttonTheme: ButtonThemeData(
+          colorScheme: ColorScheme.light(primary: primaryColor),
+          buttonColor: primaryColor,
+          splashColor: primaryColorLight,
+          padding: EdgeInsets.symmetric(vertical: 10, horizontal: 20),
+          textTheme: ButtonTextTheme.primary,
+          shape:
+              RoundedRectangleBorder(borderRadius: BorderRadius.circular(20)))
+      //fontFamily: 'Montserrat',
+      );
 }
---
+++ test/presentation/presenters/stream_login_presenter.dart
@@ -0,0 +1,28 @@
+import 'package:faker/faker.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:mockito/mockito.dart';
+import 'package:meta/meta.dart';
+
+abstract class Validation {
+  String validate({@required String field, @required String value});
+}
+
+class StreamLoginPresenter {
+  final Validation validation;
+  StreamLoginPresenter({@required this.validation});
+  void validateEmail(String email) {
+    validation.validate(field: 'email', value: email);
+  }
+}
+
+class ValidationSpy extends Mock implements Validation {}
+
+void main() {
+  test('Should call validation with correct email', () {
+    final validation = ValidationSpy();
+    final email = faker.internet.email();
+    final sut = StreamLoginPresenter(validation: validation);
+    sut.validateEmail(email);
+    verify(validation.validate(field: 'email', value: email)).called(1);
+  });
+}
