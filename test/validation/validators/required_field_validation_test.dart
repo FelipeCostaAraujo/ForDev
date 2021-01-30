f0744a6e0167d5d4d38ff8169f4f344bb265acb0
Felipe C. Araujo <felipe.s-04@hotmail.com>
2021-01-29 22:35:01 -0300

    ensure RequiredFieldValidation returns error if value is empty

--- test/validation/validators/required_field_validation_test.dart
+++ test/validation/validators/required_field_validation_test.dart
@@ -11,16 +11,20 @@ class RequiredFieldValidation implements FieldValidation{
   RequiredFieldValidation(this.field);
 
   String validate(String value){
-    return null;
+    return value.isEmpty ? "Campo obrigatório" : null;
   }
 }
 
 void main(){
   test('Should return null if value is not empty', (){
     final sut = RequiredFieldValidation('any_field');
-
     final error = sut.validate('any value');
-
     expect(error, null);
+  });
+
+  test('Should error if value is empty', (){
+    final sut = RequiredFieldValidation('any_field');
+    final error = sut.validate('');
+    expect(error, "Campo obrigatório");
   });
 }
