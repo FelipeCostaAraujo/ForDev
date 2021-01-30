f6796d1463e410f5fe1e83efa0c85a99007799e1
Felipe C. Araujo <felipe.s-04@hotmail.com>
2021-01-29 23:22:05 -0300

    ensure emailValidation returns null if email is empty and null

---
+++ test/validation/validators/email_validation_test.dart
@@ -0,0 +1,30 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:for_dev/validation/protocols/field_validation.dart';
+
+class EmailValidation implements FieldValidation {
+  final String field;
+
+  EmailValidation(this.field);
+
+  String validate(String value) {
+    return null;
+  }
+}
+
+void main() {
+  EmailValidation sut;
+
+  setUp(() {
+    sut = EmailValidation('any_field');
+  });
+
+  test('Should return null if email is empty', () {
+    expect(sut.validate(''), null);
+  });
+
+  test('Should return null if email is null', () {
+    expect(sut.validate(null), null);
+  });
+}
+
+// RegExp(r"^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,253}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,253}[a-zA-Z0-9])?)*$")
