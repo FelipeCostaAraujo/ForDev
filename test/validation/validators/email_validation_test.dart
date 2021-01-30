4da5745b7e10059d1a4e75b9b34f19f4c3d44933
Felipe C. Araujo <felipe.s-04@hotmail.com>
2021-01-29 23:42:07 -0300

    ensure emailValidation returns error if email is invalid

--- test/validation/validators/email_validation_test.dart
+++ test/validation/validators/email_validation_test.dart
@@ -7,7 +7,10 @@ class EmailValidation implements FieldValidation {
   EmailValidation(this.field);
 
   String validate(String value) {
-    return null;
+    final regex =
+      RegExp(r"^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,253}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,253}[a-zA-Z0-9])?)*$");
+    final isValid = value?.isNotEmpty != true  || regex.hasMatch(value);
+    return isValid ? null : "Campo inválido";
   }
 }
 
@@ -25,6 +28,14 @@ void main() {
   test('Should return null if email is null', () {
     expect(sut.validate(null), null);
   });
+
+  test('Should return null if email is valid', () {
+    expect(sut.validate('felipe@futuroallu.com'), null);
+  });
+
+  test('Should return error if email is invalid', () {
+    expect(sut.validate('any_invalid_email'), "Campo inválido");
+  });
 }
 
-// RegExp(r"^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,253}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,253}[a-zA-Z0-9])?)*$")
+//
