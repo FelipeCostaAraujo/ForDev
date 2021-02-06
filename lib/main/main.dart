993497c7fdcdedf19413cac9f3f8a6c899303444
Felipe C. Araujo <felipe.s-04@hotmail.com>
2021-02-05 23:38:04 -0300

    test change theme with getx

--- lib/main/main.dart
+++ lib/main/main.dart
@@ -1,30 +1,56 @@
 import 'package:flutter_dotenv/flutter_dotenv.dart' as DotEnv;
 import 'package:flutter/material.dart';
+import 'package:for_dev/ui/pages/home/home_page.dart';
 import 'package:get/get.dart';
+import 'package:get_storage/get_storage.dart';
 
 import './factories/factories.dart';
 import '../ui/themes/themes.dart';
 
 void main() async{
   await DotEnv.load(fileName: ".env");
+  await GetStorage.init();
   runApp(App());
 }
 
 class App extends StatelessWidget {
+
+  final appdata = GetStorage();
+
   @override
   Widget build(BuildContext context) {
+    appdata.writeIfNull('darkmode', false);
+    bool isDarkMode = appdata.read('darkmode');
+    Get.changeThemeMode(isDarkMode ? ThemeMode.dark : ThemeMode.light);
+
     return Container(
         child: GetMaterialApp(
           title: "4Dev",
           debugShowCheckedModeBanner: false,
           theme: lightTheme(),
+          darkTheme: darkTheme(),
           initialRoute: '/',
           getPages: [
             GetPage(name: '/', page: makeSplashPage, transition: Transition.fade),
             GetPage(name: '/login', page: makeLoginPage, transition: Transition.fadeIn),
-            GetPage(name: '/home', page: () => Scaffold(body: Text("Essa e a home"),) , transition: Transition.fadeIn)
+            GetPage(name: '/home', page: () => HomePage(), transition: Transition.fadeIn)
           ],
         )
-    );
+    );;
   }
 }
+
+
+
+// Scaffold(
+// body: Center(
+// child: Column(
+// children: [
+// SizedBox(
+// height: 40,
+// ),
+// Text("Essa e a home"),
+// ],
+// ),
+// )
+// )
---
+++ lib/ui/pages/home/home_page.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:get_storage/get_storage.dart';
+
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  final appdata = GetStorage();
+
+  @override
+  Widget build(BuildContext context) {
+    appdata.writeIfNull('darkmode', false);
+
+    return SimpleBuilder(
+        builder: (_){
+          bool isDarkMode = appdata.read('darkmode');
+
+          return Scaffold(
+              body: Center(
+                child: Column(
+                  children: [
+                    SizedBox(
+                      height: 40,
+                    ),
+                    Text("Essa e a home"),
+                    Switch(
+                      value: isDarkMode,
+                      onChanged: (value){
+                        Get.changeThemeMode(Get.isDarkMode ? ThemeMode.light : ThemeMode.dark);
+                        setState(() {
+                          print(value);
+                          appdata.write('darkmode', value);
+                          isDarkMode = value;
+                        });
+                      },
+                    ),
+                  ],
+                ),
+              )
+          );
+        }) ;
+  }
+}
